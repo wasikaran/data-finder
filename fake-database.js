9baf0dfb9d390b04a006a5e9d9ab128760942514
Wasi Karan <brainmaster816@gmail.com>
2025-07-16 22:58:06 -0700

    final commit

--- fake-database.js
+++ fake-database.js
@@ -10,7 +10,7 @@ const citizenDatabase = [
         maritalStatus: "Married",
         currentAddress: "House #12, Street 45, G-10/4, Islamabad",
         permanentAddress: "Village Kotli, District Sialkot",
-        image: "/images/person2.jpg",
+        image: "images/person2.jpg",
         familyMembers: [
             { name: "Ayesha Khan", relation: "Wife", cnic: "42201-7654321-9" },
             { name: "Ahmed Khan", relation: "Son", cnic: "42201-1122334-1" },
@@ -27,7 +27,7 @@ const citizenDatabase = [
         maritalStatus: "Single",
         currentAddress: "Flat #305, Gulberg Heights, Lahore",
         permanentAddress: "House #45, Model Town, Lahore",
-        image: "/images/person1.jpg",
+        image: "images/person1.jpg",
         familyMembers: [
             { name: "Muhammad Aslam", relation: "Father", cnic: "35202-1234567-3" },
             { name: "Amina Bibi", relation: "Mother", cnic: "35202-2345678-4" }
@@ -43,7 +43,7 @@ const citizenDatabase = [
         maritalStatus: "Divorced",
         currentAddress: "Shop #12, Saddar Bazaar, Karachi",
         permanentAddress: "House #78, Nazimabad, Karachi",
-        image: "/images/person3.jpg",
+        image: "images/person3.jpg",
         familyMembers: [
             { name: "Raza Muhammad", relation: "Father", cnic: "45403-1122334-5" },
             { name: "Hassan Raza", relation: "Brother", cnic: "45403-3344556-6" }
